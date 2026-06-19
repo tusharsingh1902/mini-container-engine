880643a1f84dd17d2765e817cff4a9503c4381fd
tusharsingh1902 <101180005+tusharsingh1902@users.noreply.github.com>
2026-06-19 10:46:01 +0530

    Refactor setup.sh configuration and constants

--- setup.sh
+++ setup.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+#
+# Configuration settings for Conductor
+# Author: Tushar Singh
+#
 
-## You need to modify this as per your system, 
-## This should be the interface of your system connecting 
-## to internet. 
-DEFAULT_IFC=enp0s1 
+# ---------------------------------------------------------
+# Host Network Configuration
+# Update this to match your system's internet-facing interface
+# (You can find this by running 'ip a' on your host)
+# ---------------------------------------------------------
+DEFAULT_IFC=enp0s1
 
-
-# These are the configuration files that you may modify
+# ---------------------------------------------------------
+# Container Subnet Settings
+# Modify these only if they conflict with your host network
+# ---------------------------------------------------------
 IP4_SUBNET=192.168
-IP4_PREFIX_SIZE=24 # Size of assigned prefixes
-IP4_FULL_PREFIX_SIZE=16 # Size of IP4_SUBNET
+IP4_PREFIX_SIZE=24      # Subnet mask for individual containers
+IP4_FULL_PREFIX_SIZE=16 # Total managed address space
 
+# =========================================================
+# INTERNAL CONSTANTS (Do not modify below this line)
+# =========================================================
 
-
-# Don't modify below this line
+# Required system binaries
 NEEDED_TOOLS="ip ping iptables top debootstrap sha256sum"
+
+# Storage paths
 IMAGEDIR="$(dirname "$0")/.images"
 CONTAINERDIR="$(dirname "$0")/.containers"
 CACHEDIR="$(dirname "$0")/.cache"
+EXTRADIR="$(dirname "$0")/extras"
+
+# Reference to the main executable
 SETUP_SCRIPT="$(dirname "$0")/conductor.sh"
+
+# Global runtime state variables
 IP4_PREFIX=
 PORT=
 INNER_PORT=
@@ -26,6 +43,7 @@ OUTER_PORT=
 INTERNET=0
 EXPOSE=0
 
+# Valid base images and download endpoints
 declare -A BASE_MIRRORS=(
     [debian]="https://deb.debian.org/debian"
     [ubuntu]="http://de.archive.ubuntu.com/ubuntu"
@@ -36,7 +54,3 @@ declare -A BASE_SUITES=(
     [ubuntu:focal]="focal"
     [ubuntu:jammy]="jammy"
 )
-
-
-EXTRADIR="$(dirname "$0")/extras"
-
